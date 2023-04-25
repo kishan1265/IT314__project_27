0fd9e4bba69d43f4b376fc23371a23733482b973
kishan sangani <202001265@daiict.ac.in>
2023-04-25 21:23:05 +0530

    added admin login page

--- programming_club/routes/admin.js
+++ programming_club/routes/admin.js
@@ -17,3 +17,29 @@ router.get(
   (req, res) => res.render('admin')
   //console.log(req.user)
 );
+
+//post for admin login
+router.post('/', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      req.flash('error_msg', 'Invalid username or password');
+      return res.redirect('/admin/');
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('user', JSON.stringify(user));
+      }
+      //localStorage.setItem('user', JSON.stringify(user));
+      //
+      return res.redirect('/admin/dashboard');
+    });
+  })(req, res, next);
+});
+
+module.exports = router;
