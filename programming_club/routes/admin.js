40f6b66f7904324143d04f66cd3c8f1078030d1a
architsinghhh <singharchit16163@gmail.com>
2023-04-26 14:10:13 +0530

    get participate id

--- programming_club/routes/admin.js
+++ programming_club/routes/admin.js
@@ -11,6 +11,21 @@ router.use(bodyParser.json({ type: 'application/json' }));
 //Load User model
 const User = require('../models/User');
 
+//get paticipate ID
+router.get('/participate/:id', async function (req, res) {
+  const event_id = req.params.id;
+
+  //console.log(event_id);
+
+  const data = await Event.find({ _id: event_id }).populate('participants');
+  //console.log(data);
+  res.render('participate', {
+    event_name: data[0].name,
+    participants: data[0].participants,
+    user_name: req.user.name,
+  });
+});
+
 //get for admin login
 router.get(
   '/',
